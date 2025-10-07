662b5a3e8b6d36c1e21b7da21291b58d2066e511
Shmatoq <andronik.1769@gmail.com>
2025-10-07 18:59:57 +0300

    Setup

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,9 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='index'),
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('user_home/', views.user_home, name='user_home'),
     path('admin_home/', views.admin_home, name='admin_home'),
+    path('cart/', views.cart_detail, name='cart_detail'),
+    path('cart/add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('cart/remove/<int:product_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('cart/update/<int:product_id>/<int:quantity>/', views.update_cart, name='update_cart'),
+     path('checkout/', views.checkout_view, name='checkout'),
 ]
--- settings/urls.py
+++ settings/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("Home page")
+from accounts import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('', home, name='home'),
+    path('', views.home, name='home'),
+    path('products/', include('products.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
