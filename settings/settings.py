293b12bae29df465a22dfa5dcb6da6ecf40ab367
Shmatoq <151510927+TimonAndronik@users.noreply.github.com>
2025-10-14 22:14:47 +0300

    Update settings.py

--- settings/settings.py
+++ settings/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c=oq5p$2ly42ca9gi4#p+5z0bo9m1f)+yitn^=gd!-f0u_4=_s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
