5300fe581407144756a1f9c2b7abcb5703391c85
Shmatoq <151510927+TimonAndronik@users.noreply.github.com>
2025-10-15 11:49:42 +0300

    Update models.py

--- products/models.py
+++ products/models.py
@@ -1,4 +1,6 @@
+# products/models.py
 from django.db import models
+from django.conf import settings
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
@@ -8,3 +10,13 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    text = models.TextField()
+    rating = models.PositiveSmallIntegerField(default=5)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Відгук від {self.user.username} для {self.product.name}"
