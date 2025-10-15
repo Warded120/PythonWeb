b30b691f9e8b21b76eb3e3650c7008872cbac9d8
Shmatoq <151510927+TimonAndronik@users.noreply.github.com>
2025-10-15 11:50:19 +0300

    Update views.py

--- products/views.py
+++ products/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from .models import Product
-from django.http import JsonResponse
+# products/views.py
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Product, Review
+from .forms import ReviewForm
 
-def product_list(request):
-    products = Product.objects.all()
-    return render(request, 'products/product_list.html', {'products': products})
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    reviews = product.reviews.all()
 
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.product = product
+            review.user = request.user
+            review.save()
+            return redirect('product_detail', pk=product.pk)
+    else:
+        form = ReviewForm()
+
+    return render(request, 'products/product_detail.html', {
+        'product': product,
+        'reviews': reviews,
+        'form': form
+    })
