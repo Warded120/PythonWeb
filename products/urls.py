eec0f4192f2a08f16e01fb6ef198d272e8d8abb4
Shmatoq <151510927+TimonAndronik@users.noreply.github.com>
2025-10-15 11:58:53 +0300

    Update urls.py

--- products/urls.py
+++ products/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('', views.product_list, name='product_list'),  
     path('<int:pk>/', views.product_detail, name='product_detail'),
-    
+    path('<int:pk>/add_review/', views.add_review, name='add_review'),
 ]
-
