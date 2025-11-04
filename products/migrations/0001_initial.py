35ddfe8b3a4fd90405649083f67b14dd5677bed2
Warded120 <greenvania11@gmail.com>
2025-11-04 09:47:16 +0200

    fix migrations

--- products/migrations/0001_initial.py
+++ products/migrations/0001_initial.py
@@ -18,6 +18,7 @@ class Migration(migrations.Migration):
                 ('name', models.CharField(max_length=100)),
                 ('description', models.TextField()),
                 ('price', models.DecimalField(decimal_places=2, max_digits=8)),
+                ('image', models.ImageField(blank=True, null=True, upload_to='products/')),
             ],
         ),
     ]
