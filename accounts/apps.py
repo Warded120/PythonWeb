0745cb91fd148d05c004da84ece11e2df4add4b5
Warded120 <greenvania11@gmail.com>
2025-11-04 09:35:11 +0200

    add migrations on startup

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,6 +1,17 @@
 from django.apps import AppConfig
+from django.core.exceptions import ImproperlyConfigured
+from django.core.management import call_command
+from django.db import OperationalError
 
 
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+
+    def ready(self):
+        try:
+            call_command('migrate', '--noinput')
+            call_command('loaddata', 'accounts')
+            print("✅ Account fixtures loaded.")
+        except (OperationalError, ImproperlyConfigured):
+            pass
